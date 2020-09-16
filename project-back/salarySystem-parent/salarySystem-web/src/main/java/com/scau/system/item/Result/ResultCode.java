44216138be0d3985e62bde8c54e10ce9d714a96d
QTJ <853890233@qq.com>
2020-09-16 10:17:38 +0800

    增加了result包，增加了.gitignore文件

---
+++ project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/system/item/Result/ResultCode.java
@@ -0,0 +1,11 @@
+package com.scau.system.item.Result;
+
+/**
+ * @Author: qtj
+ * @Date: 2020/9/16 10:16
+ * @Version
+ */
+public interface ResultCode {
+    public static Integer SUCCESS=20000; //成功
+    public static Integer ERROR=20001;   //失败
+}
