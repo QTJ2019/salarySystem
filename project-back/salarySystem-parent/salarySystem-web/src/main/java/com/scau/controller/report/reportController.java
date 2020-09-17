66d9e82cde2f31f8f44038f49591458a929ca222
QTJ <853890233@qq.com>
2020-09-17 15:20:02 +0800

    1、实现工资查询报表导出成excel的功能

--- project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/controller/report/reportController.java
+++ project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/controller/report/reportController.java
@@ -1,5 +1,6 @@
 package com.scau.controller.report;
 
+import com.alibaba.excel.EasyExcel;
 import com.scau.Result.Result;
 import com.scau.entity.SalaryResult;
 import com.scau.service.SalaryResultService;
@@ -9,6 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
@@ -25,6 +31,7 @@ import java.util.Map;
 public class reportController {
     @Autowired
     SalaryResultService salaryResultService;
+
     @RequestMapping("/querysalaryform")
     private Result querySalaryForm(String deptName, String start, String end, Integer employeeId){
         Result result = Result.ok();
@@ -60,4 +67,46 @@ public class reportController {
      return result;
     }
 
+    @RequestMapping("/exportsalaryform")
+    private void exportSalaryForm(String deptName, String start, String end, Integer employeeId, HttpServletResponse response){
+        Result result = Result.error();
+        List<SalaryResult> resultList = null;
+        Date startDate=null;
+        Date endDate=null;
+        try {
+            if (start != null){
+                startDate = CalendarUtil.getFirstDateOfMonth(start);
+            }
+            if (end != null){
+                endDate = CalendarUtil.getLastDateOfMonth(end);
+            }
+            resultList = salaryResultService.querrySalaryForm(deptName,
+                                                             startDate,
+                                                             endDate,
+                                                             employeeId);
+        }catch (ParseException e){
+            return ;
+        }
+        catch (Exception e){
+            return ;
+        }
+
+        String fileName = null;
+        try {
+            fileName = URLEncoder.encode("工资报表","utf-8");
+        } catch (UnsupportedEncodingException e) {
+            result = Result.error();
+            return ;
+        }
+        response.setContentType("application/vnd.ms-excel");
+        response.setContentType("utf-8");
+        response.setHeader("Content-disposition","attachment;filename="+fileName+".xlsx");
+        try {
+            EasyExcel.write(response.getOutputStream(),SalaryResult.class).sheet("sheet1").doWrite(resultList);
+        } catch (IOException e) {
+            return ;
+        }
+
+        return ;
+    }
 }
