741799d8a82881e752ea3bca2c386bea5f7ede85
wynhfy <457968028@qq.com>
2020-09-18 17:01:30 +0800

    完成了station所有的接口

--- project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/controller/infoController/StationController.java
+++ project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/controller/infoController/StationController.java
@@ -1,14 +1,19 @@
 package com.scau.controller.infoController;
 
 
+import com.alibaba.druid.support.spring.stat.annotation.Stat;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.scau.Result.Result;
+import com.scau.entity.Department;
+import com.scau.entity.Station;
+import com.scau.entity.vo.StationVO;
 import com.scau.service.DepartmentService;
 import com.scau.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -36,6 +41,67 @@ public class StationController {
     public Result addStation(MultipartFile file){
         stationService.saveStation(file,stationService,departmentService);
         return Result.ok();
+    }
+
+    /**
+     * 直接添加工种信息
+     * @param stationVO
+     * @return
+     */
+    @PostMapping("saveStation")
+    public Result saveStation(@RequestBody StationVO stationVO){
+        Station station=new Station();
+        station.setName(stationVO.getName());
+        QueryWrapper<Department> wrapper=new QueryWrapper<>();
+        wrapper.eq("name",stationVO.getDeptName());
+        Department department = departmentService.getOne(wrapper);
+        station.setDepartmentid(department.getId());
+        boolean flag=stationService.save(station);
+        if(flag){
+            return Result.ok();
+        }else{
+            return Result.error();
+        }
+    }
+
+    /**
+     * 删除工种
+     * @param id
+     * @return
+     */
+    @GetMapping("removeStation/{id}")
+    public Result removeStation(@PathVariable("id") Integer id){
+        boolean flag=stationService.removeById(id);
+        if(flag){
+            return Result.ok();
+        }else{
+            return Result.error();
+        }
+    }
+
+    /**
+     * 获取所有的工种
+     * @return
+     */
+    @GetMapping("getAllStation")
+    public Result getAllStation(){
+        List<Station> stationList=stationService.list(null);
+        return Result.ok().data("items",stationList);
+    }
+
+    /**
+     * 修改工种信息
+     * @param station
+     * @return
+     */
+    @PostMapping("updateStation")
+    public Result updateStation(@RequestBody Station station){
+        boolean flag=stationService.updateById(station);
+        if(flag){
+            return Result.ok();
+        }else{
+            return Result.error();
+        }
     }
 
 
---
+++ project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/entity/vo/StationVO.java
@@ -0,0 +1,12 @@
+package com.scau.entity.vo;
+
+import lombok.Data;
+
+@Data
+public class StationVO {
+
+    private String name;
+
+    private String deptName;
+
+}
