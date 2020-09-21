af0862f041c52fc54b3ca817d8c322782dddcbaf
wynhfy <457968028@qq.com>
2020-09-21 20:16:22 +0800

    新增了条件查询接口

--- project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/controller/infoController/EmployeeController.java
+++ project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/controller/infoController/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.scau.controller.infoController;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.scau.Result.Result;
 import com.scau.entity.Department;
 import com.scau.entity.Employee;
+import com.scau.entity.vo.EmployeeQuery;
 import com.scau.entity.vo.EmployeeVO;
 import com.scau.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -67,6 +70,34 @@ public class EmployeeController {
     public Result getAllEmployee(){
         List<Employee> list=employeeService.list(null);
         return Result.ok().data("list",list);
+    }
+
+
+    /**
+     * 根据条件查询员工信息
+     * @param employeeQuery
+     * @return
+     */
+    @PostMapping("getEmployeeByCondition")
+    public Result getEmployeeByCondition(@RequestBody(required = false) EmployeeQuery employeeQuery){
+        String name=employeeQuery.getName();
+        String deptName=employeeQuery.getDeptName();
+        String stationName=employeeQuery.getStationName();
+        QueryWrapper<Employee> wrapper=new QueryWrapper<>();
+        if(StringUtils.isEmpty(name)){
+            wrapper.eq("name",name);
+        }
+        if(StringUtils.isEmpty(deptName)){
+            wrapper.eq("departmentName;",deptName);
+        }
+        if(StringUtils.isEmpty(stationName)){
+            wrapper.eq("stationName",stationName);
+        }
+        Employee employee = employeeService.getOne(wrapper);
+        if(employee==null){
+            log.error("该员工不存在");
+        }
+        return Result.ok().data("employee",employee);
     }
 
     /**
---
+++ project-back/salarySystem-parent/salarySystem-web/src/main/java/com/scau/entity/vo/EmployeeQuery.java
@@ -0,0 +1,15 @@
+package com.scau.entity.vo;
+
+
+import lombok.Data;
+
+@Data
+public class EmployeeQuery {
+
+    private String name;
+
+    private String deptName;
+
+    private String stationName;
+
+}
